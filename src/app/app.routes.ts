2f58917848b22ce5cca7a0a232da1523039cea5d
Mangaluxe <haiou@mangaluxe.com>
2024-10-24 16:52:53 +0200

    css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    }
+    {path: '', component: HomeComponent}, // http://localhost:4200/
+    {path: 'recette', component: RecipeComponent} // http://localhost:4200/recette
+
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,23 +6,32 @@ import { UsersService } from '../../utils/services/users/users.service';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  // styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+
+  // ----- Propriétés -----
 
   text: string = ""
 
-  constructor(private usersService: UsersService){}
 
-  // ngOnInit(): void {
-  //   this.usersService.doHello().subscribe({
-  //     next: (res) => {
-  //       this.text = res
-  //     },
-  //     error: (err) => {
-  //       console.log("Error " +  JSON.stringify(err))
-  //     }
-  //   });
-  // }
-  
+  // ----- Constructeur -----
+
+  constructor(private usersService: UsersService) {}
+
+
+  // ----- Méthodes -----
+
+  ngOnInit(): void {
+    // this.usersService.doHello().subscribe({
+    //   next: (res) => {
+    //     this.text = res
+    //   },
+    //   error: (err) => {
+    //     console.log("Error " +  JSON.stringify(err))
+    //   }
+    // });
+  }
+
+
 }
